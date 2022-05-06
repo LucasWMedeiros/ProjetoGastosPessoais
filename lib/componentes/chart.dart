3e9ad47d6c0e38fe21a193ec13edc75130e93140
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-05-06 14:13:33 -0300

    criando a chartbar

--- lib/componentes/chart.dart
+++ lib/componentes/chart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:projeto_gastos/componentes/chartbar.dart';
 import 'package:projeto_gastos/model/transaction.dart';
 
 class Chart extends StatelessWidget {
@@ -21,28 +22,39 @@ class Chart extends StatelessWidget {
         if(sameDay && sameMonth && sameYear){
           totalSum += recentTransactions[i].value;
         }
-
-        print(DateFormat.E().format(weekDay)[0]);
-        print(totalSum);
       }
 
       return {
         'day': DateFormat.E().format(weekDay)[0],
-        'value': 9.99,
+        'value': totalSum,
       };
+    });
+  }
+
+  double get _weekTotalValue{
+    return groupedTransactions.fold(0.0, (sum, tr) {
+      return sum + (tr['value'] as double);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    groupedTransactions;
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactions.map((tr){
-          return Text('${tr['day']} : ${tr['value']}');
-        }).toList(),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactions.map((tr){
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(label: tr['day'], 
+              value: tr['value'], 
+              percentage: (tr['value'] as double) / _weekTotalValue,),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
--- lib/componentes/chartbar.dart
+++ lib/componentes/chartbar.dart
@@ -1,8 +1,50 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
+  final label;
+  final value;
+  final double? percentage;
+
+  ChartBar({this.label, this.value, this.percentage});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        FittedBox(
+          child: Text('${value.toStringAsFixed(2)}')
+          ),
+        SizedBox(
+          height: 5,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1.0),
+                      color: Color.fromRGBO(220, 220, 220, 1),
+                      borderRadius: BorderRadius.circular(5))),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+              )
+            ],
+          ),
+        ),
+        Text(label)
+      ],
+    );
   }
 }
