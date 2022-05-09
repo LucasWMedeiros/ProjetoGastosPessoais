b58bba245b9442f6c29586870bf8d8ec1867c60e
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-05-09 14:23:37 -0300

    finalizando aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -71,6 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     Navigator.of(context).pop();
   }
+  
     _removeTransaction(String id){
       setState(() {
         _transaction.removeWhere((tr) => tr.id == id);
