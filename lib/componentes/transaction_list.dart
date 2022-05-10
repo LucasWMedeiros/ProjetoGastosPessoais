1970120a337fea79d7905b1f18aa6c454ef5cb8c
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-05-10 13:58:59 -0300

    trabalhando na responsividade do app

--- lib/componentes/transaction_list.dart
+++ lib/componentes/transaction_list.dart
@@ -13,58 +13,55 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: transactions.isEmpty ? Column(
-        children: [
-          Text('Nenhuma Transação Cadastrada',
-          style: Theme.of(context).textTheme.headline6,
+    return transactions.isEmpty ? Column(
+      children: [
+        Text('Nenhuma Transação Cadastrada',
+        style: Theme.of(context).textTheme.headline6,
+        ),
+        SizedBox(height: 20),
+        Container(
+          height: 250,
+          child: Image.asset(
+            'assets/images/waiting.png',
+            fit: BoxFit.cover,
           ),
-          SizedBox(height: 20),
-          Container(
-            height: 250,
-            child: Image.asset(
-              'assets/images/waiting.png',
-              fit: BoxFit.cover,
-            ),
-          )
-        ],
-      ) : ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          final tr = transactions[index];
-          return Card(
-            elevation: 5,
-            margin: EdgeInsets.symmetric(
-              vertical: 8,
-              horizontal: 5
-            ),
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.amber,
-                radius: 30,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: FittedBox(child: Text('R\$${tr.value}',
-                   style: TextStyle(color: Colors.white),)),
-                ),
-              ),
-              title: Text(
-                tr.tittle,
-                style: Theme.of(context).textTheme.headline6,
-              ),
-              subtitle: Text(
-                DateFormat('dd MMM y').format(tr.data)
-              ),
-              trailing: IconButton(
-                icon: Icon(Icons.delete),
-                color: Theme.of(context).errorColor,
-                onPressed: () => onRemoved(tr.id),
+        )
+      ],
+    ) : ListView.builder(
+      itemCount: transactions.length,
+      itemBuilder: (ctx, index) {
+        final tr = transactions[index];
+        return Card(
+          elevation: 5,
+          margin: EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 5
+          ),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.amber,
+              radius: 30,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FittedBox(child: Text('R\$${tr.value}',
+                 style: TextStyle(color: Colors.white),)),
               ),
             ),
-          );
-        },
-      ),
+            title: Text(
+              tr.tittle,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            subtitle: Text(
+              DateFormat('dd MMM y').format(tr.data)
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              color: Theme.of(context).errorColor,
+              onPressed: () => onRemoved(tr.id),
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,30 +15,23 @@ class ExpansesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
-      theme: tema.copyWith(
-        colorScheme: tema.colorScheme.copyWith(
-        primary: Colors.amber,
-        secondary: Colors.pink,
-        ),
-        textTheme: tema.textTheme.copyWith(
-            headline6: TextStyle(
-              fontFamily: 'OpenSans',
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Colors.black
-            )
-          ),
-          appBarTheme: AppBarTheme(
-            titleTextStyle: TextStyle(
-              fontFamily: 'OpenSans',
-              fontSize: 20,
-              fontWeight: FontWeight.bold
-            )
-          )
-
-    ));
-        
+        home: MyHomePage(),
+        theme: tema.copyWith(
+            colorScheme: tema.colorScheme.copyWith(
+              primary: Colors.amber,
+              secondary: Colors.pink,
+            ),
+            textTheme: tema.textTheme.copyWith(
+                headline6: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black)),
+            appBarTheme: AppBarTheme(
+                titleTextStyle: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold))));
   }
 }
 
@@ -48,15 +41,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _transaction = [
-   
-  ];
+  final List<Transaction> _transaction = [];
 
-  List<Transaction> get _recentTransactions{
-    return _transaction.where((tr){
+  List<Transaction> get _recentTransactions {
+    return _transaction.where((tr) {
       return tr.data.isAfter(DateTime.now().subtract(Duration(days: 7)));
     }).toList();
   }
+
   void _addTransaction(String tittle, double value, DateTime date) {
     final newTransaction = Transaction(
         id: Random().nextDouble().toString(),
@@ -68,15 +60,14 @@ class _MyHomePageState extends State<MyHomePage> {
       _transaction.add(newTransaction);
     });
 
-
     Navigator.of(context).pop();
   }
-  
-    _removeTransaction(String id){
-      setState(() {
-        _transaction.removeWhere((tr) => tr.id == id);
-      });
-    }
+
+  _removeTransaction(String id) {
+    setState(() {
+      _transaction.removeWhere((tr) => tr.id == id);
+    });
+  }
 
   _opneTransactionForm(BuildContext context) {
     showModalBottomSheet(
@@ -88,23 +79,34 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: Text('Despesas Pessoais'),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () => _opneTransactionForm(context),
+        )
+      ],
+    );
+
+    final avaliableHeight = MediaQuery.of(context).size.height -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () => _opneTransactionForm(context),
-          )
-        ],
-      ),
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           // ignore: prefer_const_literals_to_create_immutables
           children: [
-            Chart(_recentTransactions),
-            TransactionList(_transaction, _removeTransaction),
+            Container(
+                height: avaliableHeight * 0.3,
+                child: Chart(_recentTransactions)),
+            Container(
+              height: avaliableHeight * 0.7,
+              child: TransactionList(_transaction, _removeTransaction),
+            ),
           ],
         ),
       ),
