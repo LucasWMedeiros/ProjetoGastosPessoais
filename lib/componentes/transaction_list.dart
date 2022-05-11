74af37d71e22f70513ce6e8a7b43cd86d88ba266
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-05-11 11:59:40 -0300

    corrigindo erros de compatibilidade com modo paisagem

--- lib/componentes/transaction_list.dart
+++ lib/componentes/transaction_list.dart
@@ -13,21 +13,26 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return transactions.isEmpty ? Column(
-      children: [
-        Text('Nenhuma Transação Cadastrada',
-        style: Theme.of(context).textTheme.headline6,
-        ),
-        SizedBox(height: 20),
-        Container(
-          height: 250,
-          child: Image.asset(
-            'assets/images/waiting.png',
-            fit: BoxFit.cover,
+    return transactions.isEmpty
+     ? LayoutBuilder(
+       builder: (context, constraints){
+       return Column(
+        children: [
+          Text('Nenhuma Transação Cadastrada',
+          style: Theme.of(context).textTheme.headline6,
           ),
-        )
-      ],
-    ) : ListView.builder(
+          SizedBox(height: 20),
+          Container(
+            height: constraints.maxHeight * 0.6,
+            child: Image.asset(
+              'assets/images/waiting.png',
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+         );
+       }
+     ) : ListView.builder(
       itemCount: transactions.length,
       itemBuilder: (ctx, index) {
         final tr = transactions[index];
