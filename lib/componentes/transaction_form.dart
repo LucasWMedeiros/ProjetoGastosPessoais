d530823844d4d6ffd5d32839d9431389960d0824
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-05-11 12:46:33 -0300

    corrigindo responsividade de transaction_form

--- lib/componentes/transaction_form.dart
+++ lib/componentes/transaction_form.dart
@@ -46,60 +46,69 @@ class _TransactionFormState extends State<TransactionForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        elevation: 5,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              TextField(
-                controller: _tittleController,
-                decoration: InputDecoration(
-                  labelText: 'Titulo',
+    double bottomPading = MediaQuery.of(context).viewInsets.bottom;
+    print(MediaQuery.of(context).viewInsets.bottom);
+    return SingleChildScrollView(
+      child: Card(
+          elevation: 5,
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: 10,
+              right: 10,
+              left: 10,
+              bottom: 10 +  MediaQuery.of(context).viewInsets.bottom
+              ),
+            child: Column(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                TextField(
+                  controller: _tittleController,
+                  decoration: InputDecoration(
+                    labelText: 'Titulo',
+                  ),
                 ),
-              ),
-              TextField(
-                controller: _valueController,
-                decoration: InputDecoration(labelText: 'Valor R\$'),
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                onSubmitted: (value) => _submitForm(),
-              ),
-              Container(
-                height: 70,
-                child: Row(
+                TextField(
+                  controller: _valueController,
+                  decoration: InputDecoration(labelText: 'Valor R\$'),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  onSubmitted: (value) => _submitForm(),
+                ),
+                Container(
+                  height: 70,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(_selectedDate == null
+                            ? 'Nenhuma data selecionada!'
+                            : 'Data Selecionada: ${DateFormat('dd/MMM/y').format(_selectedDate!)}'),
+                      ),
+                      FlatButton(
+                          textColor: Colors.amber,
+                          onPressed: _showDatePicker,
+                          child: Text(
+                            'Selecionar Data',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ))
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Expanded(
-                      child: Text(_selectedDate == null
-                          ? 'Nenhuma data selecionada!'
-                          : 'Data Selecionada: ${DateFormat('dd/MMM/y').format(_selectedDate!)}'),
+                    RaisedButton(
+                      onPressed: _submitForm,
+                      child: Text(
+                        'Nova Transação',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      color: Colors.amber,
+                      textColor: Colors.white,
                     ),
-                    FlatButton(
-                        textColor: Colors.amber,
-                        onPressed: _showDatePicker,
-                        child: Text(
-                          'Selecionar Data',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ))
                   ],
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  RaisedButton(
-                    onPressed: _submitForm,
-                    child: Text(
-                      'Nova Transação',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    color: Colors.amber,
-                    textColor: Colors.white,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ));
+              ],
+            ),
+          )),
+    );
   }
 }
